882ea509593e51d7c02ae520cddfd9794057fdea
dsail <dsail@yandex.ru>
2022-08-12 17:51:53 +0300

    exercise 10_commit

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10
+// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число.\nИ получите на выходе вторую цифру числа.\nВыход из программы exit.");
+while (true)
+{
+    string input = Console.ReadLine();
+    if(!input.Equals("exit"))
+    Console.WriteLine("{0}->{1}", input, input[1]);
+    else
+    break;
+    
+}
